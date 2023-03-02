2e0a52aecaa38959c654342cfb6a8602f2f34074
Viraj Warhade <86669937+virajwarhade@users.noreply.github.com>
2023-03-02 19:55:08 +0530

    ui improved basic for prototype

--- chatbothome.php
+++ chatbothome.php
@@ -1,3 +1,6 @@
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,15 +8,35 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>demo</title>
+
+    
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+
+
+
+
 </head>
 <body>
 
 <form method='get'>
-<input type="text" name="question" >
-<input type="submit" name="submit" >
-</form>
 
 
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-header">
+          ChatBot
+        </div>
+        <div class="card-body">
+          <div class="chat-box" id="chat-box">
+          
+
+
+            <div class="incoming-message">
+              <p>
     <?php
 if(isset($_GET['submit'])){
     // Define the delimiter used in the data file
@@ -57,5 +80,43 @@ if(isset($_GET['submit'])){
     }
 }
     ?>
+</p>
+            </div>
+
+          </div>
+          <form id="chat-form">
+            <div class="input-group mb-3">
+              <input type="text" class="form-control" placeholder="Type your message here" aria-label="Type your message here" name="question">
+              <div class="input-group-append">
+                <button class="btn btn-primary" type="submit" name="submit">Send</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+</form>
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+  </script>
+
+
+
 </body>
 </html>
